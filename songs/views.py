ea094f8ba1d6d433921133a80a845c29f91b0615
bdavis83 <bdavis83@gmail.com>
2023-02-28 14:30:49 -0500

    created funciton songs_list with GET and POST requests.

--- songs/views.py
+++ songs/views.py
@@ -6,3 +6,19 @@ from .serializers import SongsSerializer
 from .models import Songs
 from django.shortcuts import get_object_or_404
 
+@api_view(['GET', 'POST'])
+def songs_list (request):
+    
+    if request.method == 'GET':
+        songs_var = Songs.objects.all()
+        song_serializer = SongsSerializer (songs_var, many=True)
+        custom_response_dict = {
+            'song': song_serializer.data
+        }
+        return Response (custom_response_dict)
+
+    elif request.method == 'POST':
+        serializer = SongsSerializer (data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response (serializer.data, status = status.HTTP_201_CREATED)
