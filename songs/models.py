fcffe1be5ec194e088ee4fff89de41a11bd8a1b0
bdavis83 <bdavis83@gmail.com>
2023-03-01 12:10:18 -0500

    applied migrations to get the Mysql table created in workbench

--- songs/models.py
+++ songs/models.py
@@ -7,5 +7,5 @@ class Songs (models.Model):
     title = models.CharField(max_length=255)
     artist = models.CharField(max_length=255)
     album = models.CharField(max_length=255)
-    release_date = models.DateField
+    release_date = models.DateField()
     genre = models.CharField(max_length=255)
--- songs/serializers.py
+++ songs/serializers.py
@@ -4,5 +4,5 @@ from .models import Songs
 class SongsSerializer (serializers.ModelSerializer):
     class Meta:
         model = Songs
-        fields = ['id', 'title', 'artist', 'album', 'release_date', 'genre',]
+        fields = ['id', 'title', 'artist', 'album', 'release_date', 'genre']
         depth = 1
